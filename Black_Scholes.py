f362b4a4f022c6a3019dd6aef4f792f223ac9077
Yu Zhu <44738431+monkeyYuZhu@users.noreply.github.com>
2020-06-06 22:03:21 -0400

    Update Black_Scholes.py

--- Black_Scholes.py
+++ Black_Scholes.py
@@ -11,20 +11,20 @@ from scipy.stats import norm
 
 #===================================================================================================================
 # implement the Black-Scholes formula
-def blackscholes(s0, sigma, T, t, k, r, c=False, p=False):
+def blackscholes(sigma, s0, div, tau, k, r, c=False, p=False):
     '''
     s0: current stock price s0
     sigma: volatility
-    T: end
-    t: start
+    tau: (T - t)/252 such as from 2017/6/17 to 2017/5/1 there are 47 days, annualized 47/252 year, exclude the last day 
     k: strike price
     r: short-term interest rate
     '''
-    d1 = (np.log(s0/k) + (r - sigma + 0.5*sigma**2)*(T - t))/sigma*math.sqrt((T - t)*1.0) 
-    d2 = d1 - sigma*math.sqrt(T - t)
+    delta = tau/252.0
+    d1 = (np.log(s0/k*1.0) + (r - div + 0.5*sigma**2)*delta)/(sigma*math.sqrt(delta*1.0)*1.0) 
+    d2 = d1 - sigma*math.sqrt(delta)
     
-    call_price = s0*math.exp(-sigma*(T - t))*norm.cdf(d1) - k*math.exp(-r*(T - t))*norm.cdf(d2) 
-    put_price = k*math.exp(-r*(T - t))*norm.cdf(-d2) - s0*math.exp(-sigma*(T - t))*norm.cdf(-d1)
+    call_price = s0*math.exp(-div*delta)*float(norm.cdf(d1)) - k*math.exp(-r*delta)*float(norm.cdf(d2)) 
+    put_price = k*math.exp(-r*delta)*float(norm.cdf(-d2)) - s0*math.exp(-div*delta)*float(norm.cdf(-d1))
     
     price = None
     if c and p:
@@ -38,13 +38,3 @@ def blackscholes(s0, sigma, T, t, k, r, c=False, p=False):
         pass
     
     return price
-
-#===================================================================================================================
-# testing
-try:
-    result = blackscholes(s0=10, sigma=0.07, T=1, t=0.5, k=5, r=0.006, c=True, p=False)
-    print(result)
-except:
-    print("Error found. ")
-finally:
-    print("Testing finished.")
